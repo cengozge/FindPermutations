b66143e2f05445fd7f69c91a6eb2685aad47b5ce
Ozge <ozge.gokay@kuehne-nagel.com>
2017-07-20 15:59:00 +0300

    --refactor

--- src/main/java/FindPermutations.java
+++ src/main/java/FindPermutations.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class FindPermutations {
 
 	public static List<String> permList = new ArrayList<String>();
-	public static String input = "abc";
+	public static String input = "abcd";
 	
 	public static void main(String[] args) {
 			
@@ -21,16 +21,20 @@ public class FindPermutations {
 
 		String substring = new String(s.substring(1));//get substring not including first char
 		String firstChar = String.valueOf(s.charAt(0));// get first char
-		String newString = substring + firstChar;// append first char to end of string
+		String newString = merge(substring, firstChar, "");// append first char to end of string
 		
 		if(!newString.equals(input)){
 			permList.add(newString);
 			
-			if(permList.size() != factorial(3))
+			if(permList.size() != factorial(input.length()))
 				findPerm(newString);
 		} else{
-			findPerm(String.valueOf(input.charAt(1)) + String.valueOf(input.charAt(0)) + input.substring(2, input.length()));
+			findPerm(merge(String.valueOf(input.charAt(1)) ,String.valueOf(input.charAt(0)) ,input.substring(2, input.length())));
 		}
+	}
+	
+	public static String merge(String s1, String s2, String s3){
+		return s1+s2+s3;
 	}
 	
 	public static int factorial(int i){
